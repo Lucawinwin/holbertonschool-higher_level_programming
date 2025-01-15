7883a19530ee28a808d55ce5a3fcce00b8195196
Lucawinwin <windwehrluca@gmail.com>
2025-01-15 10:28:38 +0100

    10h23

--- python-if_else_loops_functions/3-print_alphabt.py
+++ python-if_else_loops_functions/3-print_alphabt.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
-
-for letter in range(97, 123):  # ASCII values for 'a' to 'z'
-    if letter != 101 and letter != 113:  # Skip 'e' (ASCII 101) and 'q' (ASCII 113)
-        print(chr(letter), end="")
-
+for i in range(ord('a'), ord('{')):
+    if i == 113 or i == 101:
+        continue
+    print("{0:s}".format(chr(i)), end='')
--- python-if_else_loops_functions/4-print_hexa.py
+++ python-if_else_loops_functions/4-print_hexa.py
@@ -1,5 +1,3 @@
 #!/usr/bin/python3
-
-for i in range(99):  # Loop through numbers 0 to 98
-    print(f"{i} = {hex(i)}")
-
+for i in range(0, 99):
+    print("{:d} = {:s}".format(i, hex(i)))
