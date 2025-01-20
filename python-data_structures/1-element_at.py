2864423b1c59b33083bf667e4acc0ba9cd77a99c
Lucawinwin <windwehrluca@gmail.com>
2025-01-20 11:52:05 +0100

    propro1

---
+++ python-data_structures/1-element_at.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+my_list = [1, 2, 3, 4, 5]
+idx = 3
+print("Element at index {:d} is {}".format(idx, element_at(my_list, idx)))
+idx = 5
+print("Element at index {:d} is {}".format(idx, element_at(my_list, idx)))
+idx = -1
+print("Element at index {:d} is {}".format(idx, element_at(my_list, idx)))
