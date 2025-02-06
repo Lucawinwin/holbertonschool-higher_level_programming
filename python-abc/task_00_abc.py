44680853baa7ead11f679c0bd5daedb6ab49d1cf
Lucawinwin <windwehrluca@gmail.com>
2025-02-06 11:01:01 +0100

    si nos estas

--- python-abc/task_00_abc.py
+++ python-abc/task_00_abc.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env 
-from abc import ABC, 
+from abc import ABC, abstractmethod
 
 
 class Animal(ABC)
-@abstractmethod
-def sound(self):
+    @abstractmethod
+    def sound(self):
     pass
 
 class Dog(Animal):
