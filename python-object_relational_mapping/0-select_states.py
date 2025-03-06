6e3c53f4505205f02e19b39440eeb925822de582
Lucawinwin <windwehrluca@gmail.com>
2025-03-06 12:21:48 +0100

    12h22

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -1,46 +1,16 @@
 #!/usr/bin/python3
-"""
-script that lists all states from the database hbtn_0e_0_usa
-"""
-import sys
-import MySQLdb
+"""lists all states from the database hbtn_0e_0_usa"""
 
+if __name__ == '__main__':
 
-def main():
-    # Checking arguments
-    if len(sys.argv) != 4:
-        print("Usage: ./script.py <mysql_username> <mysql_password> <database_name>")
-        return
+    import MySQLdb
+    import sys
 
-    # Retrieving arguments
-    mysql_username = sys.argv[1]
-    mysql_password = sys.argv[2]
-    database_name = sys.argv[3]
+    db = MySQLdb.connect(host='localhost', port=3306,
+                         user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
 
-    try:
-        # Database connexion
-        db = MySQLdb.connect(host="localhost", user=mysql_username, passwd=mysql_password, db=database_name, port=3306)
-
-        # Cursor creation
-        cursor = db.cursor()
-
-        # Execute SQL query to retrieve reports sorted by id
-        cursor.execute("SELECT * FROM states ORDER BY id ASC")
-
-        # Retrieving and displaying results
-        results = cursor.fetchall()
-        for state in results:
-            print(state)
-
-    except MySQLdb.Error as e:
-        print(f"Error: {e}")
-
-    finally:
-        # Close to connexion
-        if 'cursor' in locals():
-            cursor.close()
-        if 'db' in locals():
-            db.close()
-
-if __name__ == "__main__":
-    main()
+    cur = db.cursor()
+    cur.execute("SELECT * FROM states ORDER BY states.id ASC;")
+    rows = cur.fetchall()
+    for row in rows:
+        print(row)
